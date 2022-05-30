e45fd0e4c606a6727aa4138fbe39e6d81abadf94
Jesse Kopala <jdkopala@gmail.com>
2022-05-30 15:55:32 -0600

    Added assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 5);
