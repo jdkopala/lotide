a63d7560560295e00162d80c003771b83f1fb8f9
Jesse Kopala <jdkopala@gmail.com>
2022-05-31 16:00:24 -0600

    Updated eqArrays.js with test cases and a working function

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,25 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  
-}
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      result.push(true);
+    }
+  }
+  if (result.length === arr1.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [3, 2, 1]);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays(["Lighthouse", "Labs", "Rules"], ["Lighthouse", "Labs", "Rules"]), true);
+assertEqual(eqArrays(["Lighthouse", "Labs", "Rules"], ["Lighthouse", "Labs", "is good"]), false);
