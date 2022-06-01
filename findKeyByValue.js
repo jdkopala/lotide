c3d3cf8de061212e93cfc5f71d8a8b7b2a684ce7
Jesse Kopala <jdkopala@gmail.com>
2022-06-01 14:02:24 -0600

    Added findKeyByValue.js with test cases

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  for (let k in obj) {
+    if (obj[k] === val) {
+      return k;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+};
+
+const apples = {
+  red: "Red Delicious",
+  green: "Granny Smith",
+  yellow: "Golden Delicious",
+  pink: "Pink Lady",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+
+assertEqual(findKeyByValue(apples, "Pink Lady"), "pink");
+assertEqual(findKeyByValue(apples, "Granny Smith"), "green");
+assertEqual(findKeyByValue(apples, "Macintosh"), undefined);
