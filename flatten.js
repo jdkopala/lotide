010574284bf365be42f3006f4d0198c44829f6bf
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 17:45:19 -0600

    removed an errant console.log

--- flatten.js
+++ flatten.js
@@ -1,7 +1,7 @@
 const flatten = function(array) {
   let flatArr = [];
   for (let i of array) {
-    console.log(i);
+    // console.log(i);
     if (Array.isArray(i)) {
       for (let j = 0; j < i.length; j++) {
         flatArr.push(i[j]);
