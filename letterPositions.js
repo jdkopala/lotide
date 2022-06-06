931b2a6f300c5520cc5d5e36134d0bacc4c16a2a
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 17:04:10 -0600

    Refactored letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,22 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      result = false;
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${arr1} === ${arr2}`);
-  } else if (!eqArrays(arr1, arr2)) {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
 const letterPositions = function(string) {
   const result = {};
   for (let c = 0; c < string.length; c++) {
@@ -34,8 +15,4 @@ const letterPositions = function(string) {
   return result;
 };
 
-let test1 = (letterPositions("hello").l);
-let test2 = (letterPositions("lighthouse in the house").h);
-
-console.log(assertArraysEqual(test1, [2, 3]));
-console.log(assertArraysEqual(test2, [3, 5, 15, 18]));
+module.exports = letterPositions;
