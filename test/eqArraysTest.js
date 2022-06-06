6118b76fc2086aa6c299f6a65b9227603ba9e976
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 16:51:07 -0600

    Implemented mocha/chai testing for eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,20 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
+describe('#eqArrays', () => {
+  it("Should return true if the arrays match", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  })
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  it("should return false if the values are in the wrong order", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  })
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  it("should return false if the data types don't match", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  })
 
-assertEqual(eqArrays(["Lighthouse", "Labs", "Rules"], ["Lighthouse", "Labs", "Rules"]), true);
-assertEqual(eqArrays(["Lighthouse", "Labs", "Rules"], ["Lighthouse", "Labs", "is good"]), false);
+  it("Should return true if the arrays match", () => {
+    assert.deepEqual(eqArrays(["Lighthouse", "Labs", "Rules"], ["Lighthouse", "Labs", "Rules"]), true);
+  })
+});
