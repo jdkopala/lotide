3540e1e6d8afbdc020b964a5513353a58fbeafc4
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 11:14:45 -0600

    added index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map
+};
