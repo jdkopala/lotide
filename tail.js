b7a96a7296aa3ea2b1706c3713296d15d6ec849d
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 09:36:48 -0600

    fixed for loop in tail.js

--- tail.js
+++ tail.js
@@ -2,8 +2,9 @@ const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
   let tailArr = [];
-  for (let i = 1; i < arr.length; i++)
+  for (let i = 1; i < arr.length; i++) {
     tailArr.push(arr[i]);
+  };
   return tailArr;
 };
 
