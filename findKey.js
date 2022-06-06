d0b4c7579e25c47bfd105769f60748eaed2a61c9
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 17:40:45 -0600

    refactored findKey

--- findKey.js
+++ findKey.js
@@ -6,20 +6,4 @@ const findKey = function(obj, callback) {
   }
 };
 
-const constellations = {
-  "Blue Hill": {stars: 1},
-  "Akaleri": {stars: 3},
-  "noma": {stars: 2},
-  "elBulli": {stars: 3},
-  "Ora": {stars: 2},
-  "Akelarre": {stars: 3}
-};
-
-const numberOfWheels = {
-  "truck": {wheels:4},
-  "bike": {wheels:2},
-  "trike": {wheels:3},
-  "unicycle": {wheels:1}
-};
-
 module.exports = findKey;
