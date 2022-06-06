053895c53be97757f7088c94fe449b9a1e7e6e1a
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 10:39:54 -0600

    Implemented mocha test for middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,30 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+describe("#middle", () => {
+  it("should return an empty array for array with one element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("should return an empty array for array with two elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('should return [3] for [1,2,3,4,5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('should return [3,4] for [1,2,3,4,5,6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it('should return [6,7] for [1,2,3,4,5,6,7,8,9,10,11,12]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+  });
+})
+
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
