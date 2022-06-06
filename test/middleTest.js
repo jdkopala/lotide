1e1e8385d6f7ff9d082203dc83b7e9a1a33c27c0
Jesse Kopala <jdkopala@gmail.com>
2022-06-06 10:40:27 -0600

    removed old test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -23,8 +23,3 @@ describe("#middle", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
   });
 })
-
-
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]), [6, 7]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
